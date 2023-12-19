881abf0a3be61e7ae45f553e3e4c424c75a4b529
Natanael Rio Wijaya <gafterlife@gmail.com>
2023-12-19 23:16:47 +0700

    v16

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-declare const global: Global & { prisma?: PrismaClient };
+let prisma: PrismaClient;
 
-export let prisma: PrismaClient;
-
-if (typeof window === 'undefined') {
-  if (process.env['NODE_ENV'] === 'production') {
-    prisma = new PrismaClient();
-  } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
+  prisma = global.prisma;
 }
+
+export default prisma;
