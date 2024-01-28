8bd2905cf0e65baf02a79766bc33d209d207b7c2
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-01-28 15:58:46 +0700

    v49

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,11 +1,13 @@
 import styles from '@/components/introduction.module.css'
 export default function Introduction() {
     return (
-        <div className={styles.container}>
-            <div className={styles.typingeffect}>
-                Brikan ku hati seperti hatimu
+        <div className={styles.luarcontainer}>
+            <div className={styles.container}>
+                <div className={styles.typingeffect}>
+                    Pilihanmu, Pilihanku
+                </div>
+                <div className={styles.cursoreffect}>|</div>
             </div>
-            <div className={styles.cursoreffect}>|</div>
         </div>
     )
 }
