631a44edf6b6ecaea7fc68144534544de8bb9f9a
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-02-18 18:03:06 +0700

    65

--- next.config.js
+++ next.config.js
@@ -10,13 +10,27 @@ const nextConfig = {
       'shop.forem.com'
     ],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(
       new webpack.DefinePlugin({
         'process.env.FLUENTFFMPEG_COV': false
-    })
+      })
     )
- 
+
     return config
   },
   images: {
