df71d41e92c9aea23e9596d81d89633fed1fac31
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-02-18 17:39:22 +0700

    65

--- src/app/api/v1/whatapps/route.js
+++ src/app/api/v1/whatapps/route.js
@@ -1,17 +1,18 @@
 import { Client, LocalAuth } from 'whatsapp-web.js'
 
-export async function GET(req) {
+export async function POST(req) {
+    const { phoneNumber, massage } = await req.json()
     const whatsapp = new Client({
         authStrategy: new LocalAuth()
     });
 
     whatsapp.on('qr', (qr) => {
-        // Generate and scan this code with your phone
         console.log('QR RECEIVED', qr);
     });
 
     whatsapp.on('ready', () => {
         console.log('whatsapp is ready!');
+        whatsapp.sendMessage(phoneNumber, massage)
     });
 
 
@@ -22,7 +23,6 @@ export async function GET(req) {
             await message.reply('pong');
         }
     });
-
 
     whatsapp.initialize();
     return Response.json({ data: 'ok' })
