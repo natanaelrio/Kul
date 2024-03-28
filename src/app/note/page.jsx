e6349366c63f977ae11ecf3663cfe2b6157d5d3e
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-03-28 21:46:58 +0700

    v83

--- src/app/note/page.jsx
+++ src/app/note/page.jsx
@@ -3,7 +3,7 @@ import { GetListNotifikasi } from "@/utils/user-front/getListNotifikasi";
 import HeaderFooter from '@/components/Layout/headerFooter'
 
 export default async function Note() {
-    const dataList = await GetListNotifikasi('8971041460')
+    const dataList = await GetListNotifikasi(8971041460)
 
     return (
         <HeaderFooter >
