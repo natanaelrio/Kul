5aef2b0e6ef555147c37d7a05905c2174f4721bf
Natanael Rio Wijaya <gafterlife@gmail.com>
2024-03-28 22:24:12 +0700

    v83

--- src/components/listNote.jsx
+++ src/components/listNote.jsx
@@ -20,43 +20,39 @@ export default function ListNote({ dataNote }) {
                         </div>
                     </CustomBack>
                 </div>
-                <div>{dataNote?.map((data) => data?.dataPesanan)?.map((dataku) => dataku?.map((data, i) => {
-                    return (<>
-                        <div key={i} className={styles.list}>
-                            <div className={styles.atas}>
-                                <div className={styles.kiri}>
-                                    <div className={styles.icon}><FaShoppingBag className={styles.icondalam} /></div>
-                                    <div>
-                                        <div className={styles.text}>Belanja</div>
-                                        <div className={styles.tanggalpesanan}>20 maret 2024</div>
-                                    </div>
-                                </div>
-                                <div className={styles.statusperjalanan}>Selesai</div>
-                            </div>
-                            <div className={styles.tengah}>
-                                <div className={styles.gambar}>
-                                    <Image
-                                        src={`${process.env.NEXT_PUBLIC_URL}/no-image.png`}
-                                        width={50}
-                                        height={50}
-                                        alt={data?.nama_barang}
-                                    /> </div>
-                                <div className={styles.deskripsi}>
-                                    <div className={styles.judul}> {data.nama_barang_user}</div>
-                                    <div className={styles.totalbarang}> {data.jumlah_barang_user} barang</div>
+                <div>
+                    <div className={styles.list}>
+                        <div className={styles.atas}>
+                            <div className={styles.kiri}>
+                                <div className={styles.icon}><FaShoppingBag className={styles.icondalam} /></div>
+                                <div>
+                                    <div className={styles.text}>Belanja</div>
+                                    <div className={styles.tanggalpesanan}>20 maret 2024</div>
                                 </div>
                             </div>
-                            <div className={styles.bawah}>
-                                <div className={styles.totalbelanja}>
-                                    <div className={styles.text}>Total Belanja</div>
-                                    <div className={styles.total}>Rp 20.000</div>
-                                </div>
-                                <div className={styles.belilagi}>Beli Lagi</div>
+                            <div className={styles.statusperjalanan}>Selesai</div>
+                        </div>
+                        <div className={styles.tengah}>
+                            <div className={styles.gambar}>
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_URL}/no-image.png`}
+                                    width={50}
+                                    height={50}
+                                    alt={'wkwkkw'}
+                                /> </div>
+                            <div className={styles.deskripsi}>
+                                <div className={styles.judul}> </div>
+                                <div className={styles.totalbarang}> barang</div>
                             </div>
                         </div>
-
-                    </>)
-                }))}
+                        <div className={styles.bawah}>
+                            <div className={styles.totalbelanja}>
+                                <div className={styles.text}>Total Belanja</div>
+                                <div className={styles.total}>Rp 20.000</div>
+                            </div>
+                            <div className={styles.belilagi}>Beli Lagi</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
